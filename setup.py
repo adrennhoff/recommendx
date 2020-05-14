d312ad3e568d9c4dbd9021e795e9a56c96067d14
adrennhoff <adrennhoff@users.noreply.github.com>
2020-05-13 21:49:29 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='recommendx'"', 
+    packages = ['recommendx'],
+    version='0.1',
+    license = 'bsd-3-clause',
+    description = 'Recommendation system with observed attributes and time-varying coefficients',
+    author = 'Adam Rennhoff',
+    author_email = 'Adam.Rennhoff@mtsu.edu',
+    url = 'https://github.com/adrennhoff/recommendx',
+    download_url = 'X',
+    keywords = ['machine learning','recommendation system','SVD'],
+    install_requires = [
+        'numpy',
+        'pandas',
+    ]
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)
